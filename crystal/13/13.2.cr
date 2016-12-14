ab7163b2cb84ed3971d9642a615f56ce810311fd
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-13 21:28:24 -0300

    Simplify solutions for day 13

--- crystal/13/13.2.cr
+++ crystal/13/13.2.cr
@@ -1,17 +1,13 @@
-record Position, x : Int32, y : Int32, distance : Int32
-
-input = 1362
-walls = Hash({Int32, Int32}, Bool).new do |h, (x, y)|
-  next true if x < 0 || y < 0
-  h[{x, y}] = (x*x + 3*x + 2*x*y + y + y*y + input).popcount.odd?
+def wall?(x, y)
+  x < 0 || y < 0 || (x*x + 3*x + 2*x*y + y + y*y + 1362).popcount.odd?
 end
 
-steps = Deque{Position.new(1, 1, 0)}
+steps = Deque{ {1, 1, 0} }
 visited = Set{ {1, 1} }
 
 total = 0
 while pos = steps.shift?
-  x, y, dist = pos.x, pos.y, pos.distance
+  x, y, dist = pos
   break if dist == 51
 
   total += 1
@@ -20,9 +16,9 @@ while pos = steps.shift?
     {x + 1, y}, {x - 1, y},
     {x, y + 1}, {x, y - 1},
   }.each do |point|
-    next if visited.includes?(point) || walls[point]
+    next if visited.includes?(point) || wall?(*point)
     visited << point
-    steps.push Position.new(*point, dist + 1)
+    steps.push({point[0], point[1], dist + 1})
   end
 end
 puts total
