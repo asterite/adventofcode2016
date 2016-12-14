857d6932375ae1e1b3c4bdb9e099dd81ac458ffb
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-13 21:09:14 -0300

    Day 13: Crystal

---
+++ crystal/13/13.2.cr
@@ -0,0 +1,28 @@
+record Position, x : Int32, y : Int32, distance : Int32
+
+input = 1362
+walls = Hash({Int32, Int32}, Bool).new do |h, (x, y)|
+  next true if x < 0 || y < 0
+  h[{x, y}] = (x*x + 3*x + 2*x*y + y + y*y + input).popcount.odd?
+end
+
+steps = Deque{Position.new(1, 1, 0)}
+visited = Set{ {1, 1} }
+
+total = 0
+while pos = steps.shift?
+  x, y, dist = pos.x, pos.y, pos.distance
+  break if dist == 51
+
+  total += 1
+
+  {
+    {x + 1, y}, {x - 1, y},
+    {x, y + 1}, {x, y - 1},
+  }.each do |point|
+    next if visited.includes?(point) || walls[point]
+    visited << point
+    steps.push Position.new(*point, dist + 1)
+  end
+end
+puts total
