b78a95544f69c8c3bdbbe87a66e4a8b295108c03
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-16 09:34:34 -0300

    Day 16: Crystal

---
+++ crystal/16/16.2.cr
@@ -0,0 +1,16 @@
+input = "10001001100000001"
+length = 35651584
+
+a = input.each_char.map { |c| c == '1' }.to_a
+while a.size < length
+  b = a.reverse.map { |x| !x }
+  a = a + [false] + b
+end
+a = a.first(length)
+
+checksum = a
+loop do
+  checksum = checksum.each_slice(2).map { |(x, y)| x == y }.to_a
+  break if checksum.size.odd?
+end
+puts checksum.map { |x| x ? '1' : '0' }.join
