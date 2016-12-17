331f073e9d010f45eed9957f33c7d37f4e437b4c
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-17 14:30:40 -0300

    Simplify Day 17

--- crystal/17/17.1.cr
+++ crystal/17/17.1.cr
@@ -1,37 +1,21 @@
 require "crypto/md5"
 
 input = "qljzarfv"
-
 steps = Deque{ {0, 0, input, 0} }
-visited = Set{ {0, 0, input} }
-
 string = input
 while pos = steps.shift?
   x, y, string, dist = pos
   break if x == 3 && y == 3
 
-  hash = Crypto::MD5.hex_digest(string)
-  opens = hash[0...4].chars.map &.>= 'b'
+  opens = Crypto::MD5.hex_digest(string)[0...4].chars.map &.>= 'b'
 
   { {'U', 0, -1},
     {'D', 0, 1},
     {'L', -1, 0},
     {'R', 1, 0},
   }.each_with_index do |(char, dx, dy), index|
-    open = opens[index]
-    next unless open
-
-    nx = x + dx
-    next unless 0 <= nx < 4
-
-    ny = y + dy
-    next unless 0 <= ny < 4
-
-    nstring = string + char
-    next if visited.includes?({nx, ny, nstring})
-
-    visited << {nx, ny, nstring}
-    steps << {nx, ny, nstring, dist + 1}
+    next unless opens[index] && 0 <= x + dx < 4 && 0 <= y + dy < 4
+    steps << {x + dx, y + dy, string + char, dist + 1}
   end
 end
 
--- crystal/17/17.2.cr
+++ crystal/17/17.2.cr
@@ -1,10 +1,7 @@
 require "crypto/md5"
 
 input = "qljzarfv"
-
 steps = Deque{ {0, 0, input, 0} }
-visited = Set{ {0, 0, input} }
-
 max = 0
 while pos = steps.shift?
   x, y, string, dist = pos
@@ -17,25 +14,14 @@ while pos = steps.shift?
     {'L', -1, 0},
     {'R', 1, 0},
   }.each_with_index do |(char, dx, dy), index|
-    open = opens[index]
-    next unless open
+    next unless opens[index] && 0 <= x + dx < 4 && 0 <= y + dy < 4
 
-    nx = x + dx
-    next unless 0 <= nx < 4
-
-    ny = y + dy
-    next unless 0 <= ny < 4
-
-    if nx == 3 && ny == 3
+    if x + dx == 3 && y + dy == 3
       max = dist + 1 if dist + 1 > max
       next
     end
 
-    nstring = string + char
-    next if visited.includes?({nx, ny, nstring})
-
-    visited << {nx, ny, nstring}
-    steps << {nx, ny, nstring, dist + 1}
+    steps << {x + dx, y + dy, string + char, dist + 1}
   end
 end
 
