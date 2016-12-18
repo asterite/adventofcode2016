c66d9d9fcfb1732b3d298a8c376d3be842fcac81
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-18 09:16:36 -0300

    Day 18: Crystal

---
+++ crystal/18/18.2.cr
@@ -0,0 +1,18 @@
+input = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^"
+
+row = input.chars
+safe = row.count { |c| c == '.' }
+
+next_row = Array.new(row.size, '.')
+
+(400000 - 1).times do
+  row.size.times do |i|
+    left = i == 0 ? '.' : row[i - 1]
+    right = i == row.size - 1 ? '.' : row[i + 1]
+    next_row[i] = left == right ? '.' : '^'
+  end
+  row, next_row = next_row, row
+  safe += row.count { |c| c == '.' }
+end
+
+puts safe
